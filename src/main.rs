656f7ddfa6c6feb09f47d0f779e67fa08c2ffa82
Linus Göldner <Linus@goeldner-family.de>
2024-04-25 21:41:01 +0200

    day13 wip

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ const DAYS: &[Solution] = &[
     yr2022_day10::SOLUTION,
     yr2022_day11::SOLUTION,
     yr2022_day12::SOLUTION,
+	yr2022_day13::SOLUTION
 ];
 
 fn main() -> anyhow::Result<()> {
@@ -25,6 +26,7 @@ mod yr2022_day1;
 mod yr2022_day10;
 mod yr2022_day11;
 mod yr2022_day12;
+mod yr2022_day13;
 mod yr2022_day3;
 mod yr2022_day4;
 mod yr2022_day7;
