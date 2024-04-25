fd86b1b367cc4e89d7c1701ee39d243a4970b1a1
Linus Göldner <Linus@goeldner-family.de>
2024-04-25 15:30:57 +0200

    added black box to day10, finished day12, refactors
    parse now takes a const bool generic to work for both parts,
    get_adjacent now takes a bool arg for part2

--- src/main.rs
+++ src/main.rs
@@ -17,38 +17,7 @@ const DAYS: &[Solution] = &[
 ];
 
 fn main() -> anyhow::Result<()> {
-    let mut runtime = AocRuntime::new()?;
-
-    // if let Some(usr_query) = std::env::args().nth(1) {
-    //     let matcher = SkimMatcherV2::default();
-
-    //     let matched_benches = DAYS
-    //         .iter()
-    //         .flat_map(get_names)
-    //         // if the name is matched, return only the name
-    //         .filter_map(|name| matcher.fuzzy_match(&name.0, &usr_query).map(|_| name))
-    //         .collect::<Vec<_>>();
-
-    //     if matched_benches.is_empty() {
-    //         anyhow::bail!("No Matches found!");
-    //     }
-
-    //     let result = matched_benches
-    //         .into_iter()
-    //         .map(|(label, f, info)| {
-    //             let input = runtime.input_cache.get(info).unwrap();
-    //             time_bench_solution(&input, info, label, f)
-    //         })
-    //         .collect::<Vec<_>>();
-
-    //     return cli_table::print_stdout(result.with_title())
-    //         .map_err(|_| anyhow!("Failed to print table"));
-    // }
-
-    // let runs = aoc_any::bench_solutions(DAYS, &mut runtime);
-    // cli_table::print_stdout(runs.with_title()).map_err(|_| anyhow!("Failed to print table"))
-
-    runtime.run(DAYS)
+    AocRuntime::new()?.run(DAYS)
 }
 
 mod template;
